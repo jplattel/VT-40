fa7d629a91d8f9ba557ddcbcadde606ebfeacdaf
Joost Plattel <jsplattel@gmail.com>
2023-05-16 09:53:54 +0200

    updated code for the encoders to work

--- src/code.py
+++ src/code.py
@@ -54,7 +54,7 @@ keyboard.keymap = [
 		KC.ESC,  KC.Q,    KC.W,  	KC.E,   KC.R,   KC.T,   KC.Y,    KC.U,     KC.I,     KC.O,    KC.P,    KC.BKDL, 	
 		KC.TAB,  KC.A,    KC.S,  	KC.D,   KC.F,   KC.G,   KC.H,    KC.J,     KC.K,     KC.L,    KC.GRV,  KC.LSFT(KC.BSLS),
 		KC.LSFT, KC.Z,    KC.X,  	KC.C,   KC.V,   KC.B,   KC.N,    KC.M,     KC.COMMA, KC.DOT,  KC.UP,   KC.ENT,
-		KC.LCTL, KC.LALT, KC.LGUI,	KC.SPC, KC.SPC, KC.SPC, KC.RGUI, KC.RALT,  KC.NO,    KC.LEFT, KC.DOWN, KC.RIGHT,
+		KC.HYPR, KC.LCTL, KC.LALT,  KC.LGUI, KC.SPC, KC.SPC, KC.RGUI, KC.RALT,  KC.MEH,    KC.LEFT, KC.DOWN, KC.RIGHT,
 	],
 ]
 
